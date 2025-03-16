926d5a8892ddec9b7effedcdda398c3917c48c99
JettN <122491434+JettN@users.noreply.github.com>
2025-03-16 00:20:59 -0700

    moved style from innerHTML to seperate var

--- static/js/component.js
+++ static/js/component.js
@@ -6,6 +6,8 @@ class ProjectCard extends HTMLElement {
     connectedCallback() {
         // HTML and inline style as template literals for easy modification
         const style = `
+  display: flex;
+  flex-direction: column;
   border: .25rem solid black;
   margin: auto;
   padding: 2rem;
@@ -16,20 +18,14 @@ class ProjectCard extends HTMLElement {
 
         // Template for component content
         this.innerHTML = `
-  <style>
-        .container {
-                    display: flex;
-                    flex-direction: column;
-                    border: red 2px    }
-  </style>
-  <div class="container">
+  <div style="${style}">
     <h2>Pantry Pals</h2>
     <picture style="display: inline-block;">
         <source srcset="../static/images/pantry pal menu 1579.jpg" media="(min-width: 1500px)">
         <source srcset="../static/images/pantry pal menu 200.jpg" media="(min-width: 200px)">
         <img src="../static/images/pantry pal menu 1000.jpg" alt="Picture of Pantry Pal main screen">
     </picture>
-    <p style="${style}">
+    <p>
         <strong>Pantry Pals</strong> is a ChatGPT wrapper that generates recipes based on what users have in their fridge.</span>
     </p>
     <a href="project1.html">Further Reading</a>
