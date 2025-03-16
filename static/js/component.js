315dec62da30fb0ecd76b8a5827abeabe77b965b
JettN <122491434+JettN@users.noreply.github.com>
2025-03-16 00:18:21 -0700

    testing out styling the custom element

--- static/js/component.js
+++ static/js/component.js
@@ -16,16 +16,24 @@ class ProjectCard extends HTMLElement {
 
         // Template for component content
         this.innerHTML = `
-  <h2>Pantry Pals</h2>
-  <picture style="display: inline-block;">
-    <source srcset="../static/images/pantry pal menu 1579.jpg" media="(min-width: 1500px)">
-    <source srcset="../static/images/pantry pal menu 200.jpg" media="(min-width: 200px)">
-    <img src="../static/images/pantry pal menu 1000.jpg" alt="Picture of Pantry Pal main screen">
-  </picture>
-  <p style="${style}">
-    <strong>Pantry Pals</strong> is a ChatGPT wrapper that generates recipes based on what users have in their fridge.</span>
-  </p>
-  <a href="project1.html">Further Reading</a></b>
+  <style>
+        .container {
+                    display: flex;
+                    flex-direction: column;
+                    border: red 2px    }
+  </style>
+  <div class="container">
+    <h2>Pantry Pals</h2>
+    <picture style="display: inline-block;">
+        <source srcset="../static/images/pantry pal menu 1579.jpg" media="(min-width: 1500px)">
+        <source srcset="../static/images/pantry pal menu 200.jpg" media="(min-width: 200px)">
+        <img src="../static/images/pantry pal menu 1000.jpg" alt="Picture of Pantry Pal main screen">
+    </picture>
+    <p style="${style}">
+        <strong>Pantry Pals</strong> is a ChatGPT wrapper that generates recipes based on what users have in their fridge.</span>
+    </p>
+    <a href="project1.html">Further Reading</a>
+  </div>
 `;
     }
 }
