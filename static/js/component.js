4ca40393b8a8d728ae907e5d6f3beed05a096fec
JettN <122491434+JettN@users.noreply.github.com>
2025-03-16 00:05:35 -0700

    test out custom element

---
+++ static/js/component.js
@@ -0,0 +1,34 @@
+class ProjectCard extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        // HTML and inline style as template literals for easy modification
+        const style = `
+  border: .25rem solid black;
+  margin: auto;
+  padding: 2rem;
+  text-align: center;
+  width: 50vw;
+`;
+
+
+        // Template for component content
+        this.innerHTML = `
+  <h2>Pantry Pals</h2>
+  <picture style="display: inline-block;">
+    <source srcset="../static/images/pantry pal menu 1579.jpg" media="(min-width: 1500px)">
+    <source srcset="../static/images/pantry pal menu 200.jpg" media="(min-width: 200px)">
+    <img src="../static/images/pantry pal menu 1000.jpg" alt="Picture of Pantry Pal main screen">
+  </picture>
+  <p style="${style}">
+    <strong>Pantry Pals</strong> is a ChatGPT wrapper that generates recipes based on what users have in their fridge.</span>
+  </p>
+  <a href="project1.html">Further Reading</a></b>
+`;
+    }
+}
+
+// Define the custom element
+customElements.define('project-card', ProjectCard);
